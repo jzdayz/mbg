751ef8d9f13f35fe156d8ec8928642107df5e93b
huqingfeng <1397649957@qq.com>
2020-06-19 15:08:01 +0800

    0.0.4

--- src/main/java/io/github/jzdayz/mbg/exception/DefaultExceptionHandler.java
+++ src/main/java/io/github/jzdayz/mbg/exception/DefaultExceptionHandler.java
@@ -2,18 +2,21 @@ package io.github.jzdayz.mbg.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 @AllArgsConstructor
 @RestControllerAdvice
+@Slf4j
 public class DefaultExceptionHandler {
 
     private ObjectMapper objectMapper;
 
     @ExceptionHandler(Exception.class)
     public Object defaultE(Exception ex) {
+        log.error("error",ex);
         return ex;
     }
 
